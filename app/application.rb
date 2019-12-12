736f29e8582f17ea2e98796cb1429fe14c6ed2f8
Andrew Williams <1williamsaj@gmail.com>
2019-12-12 03:25:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items\/.+/)
       item = req.path.split("/items/").last
       search_item = @@items.find { |i| i.name == item_name }
-      if item
+      if search_item
         resp.write search_item.price
       else
         resp.write "Item not found"
